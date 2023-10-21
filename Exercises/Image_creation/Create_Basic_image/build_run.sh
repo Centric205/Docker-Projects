ee674305ebd9c7a5d4646f9bbcef715c5dba0ee3
Theo Madikgetla <mdkthe015@cs.uct.ac.za>
2023-10-22 00:17:59 +0200

    Added the first exercise

---
+++ Exercises/Image_creation/Create_Basic_image/build_run.sh
@@ -0,0 +1,9 @@
+# Just a script to build and run the Docker Image
+
+# 1. Build Docker image
+sudo docker build -t my-python-app .
+
+
+# 2. Run the Docker container based off the image
+sudo docker run my-python-app
+
---
+++ Exercises/Image_creation/Create_Basic_image/script.py
@@ -0,0 +1,5 @@
+# Just Random script for testing out Docker
+
+print("Hi There....(^.^)")
+print("My name is Theo Madikgetla, and I am also known as Centric")
+print("This is my very first Docker image....")
